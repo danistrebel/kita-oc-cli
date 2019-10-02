82261cbbf46c4874e85b4ff1bc3ab70687f940a9
Daniel Strebel <daniel.strebel@gmail.com>
2019-10-02 21:55:33 +0200

    add install script

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# kita-oc-cli
+# OC CLI Installer
+
+Installer script for [KITA](https://github.com/danistrebel/kita-operator) environment.
+
+## Usage
+
+```bash
+chmod +x ./install.sh
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,11 @@
+mkdir -p bin/oc
+
+curl -L https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz --output /tmp/oc-cli.tar.gz
+
+tar -xf /tmp/oc-cli.tar.gz -C ./bin/oc --strip-components 1
+
+export PATH=$(pwd)/bin/oc:$PATH
+
+SA_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
+
+oc login --token $SA_TOKEN kubernetes.default
